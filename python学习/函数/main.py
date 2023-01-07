1b7663ee0f71c0c700484dd3b3ae16726d4a5f98
2382546457 <2382546457@qq.com>
2023-01-07 21:54:57 +0800

    python学习-函数

--- python学习/函数/main.py
+++ python学习/函数/main.py
@@ -9,4 +9,19 @@ def my_len(s):
     return count
 
 
+def add(a, b):
+    return a + b
+
+
 print(my_len(name))
+result = add(10, 20)
+print(result)
+
+
+# 函数返回值 : None
+# 如果函数不使用return返回数据，默认返回None，表示: 返回了个无意义的数. 可以使用一个参数接收它，打印结果为None
+def say_hi():
+    1 + 1
+
+result1 = say_hi()
+print(result1)
